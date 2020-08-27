6dac46a8bc6bc4358343c3258d813d16b6344089
moraisgabri <gabriel8azevedo@gmail.com>
2020-08-27 20:53:41 -0300

    exercicio 8

---
+++ bloco_4/4_1/exercicio8.js
@@ -0,0 +1,13 @@
+// Escreva um programa que defina três números em variáveis no seu começo e retorne true 
+// se pelo menos uma das três for par. Caso contrário, ele retorna false.
+// Bonus: use somente um if.
+
+const num1 = 3
+const num2 = 2
+const num3 = 9
+
+if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
+  console.log('True')
+} else {
+  console.log('False')
+}
