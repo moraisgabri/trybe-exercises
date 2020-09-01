190a470117599c7c1c2b7dc3441d1306fa1045b0
moraisgabri <gabriel8azevedo@gmail.com>
2020-09-01 13:16:19 -0300

    funções exercício 2

---
+++ bloco_4/4_4/funções/exercicio2.js
@@ -0,0 +1,16 @@
+// retornar índice do maior valor de um array
+
+
+const maiorValor = function(array) {
+let containerValor = 0
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] > containerValor) {
+      containerValor = array[i]
+    }
+    
+  }
+  console.log(array.indexOf(containerValor))
+}
+
+array = [2, 3, 6, 7, 10, 1]
+maiorValor(array)
