d4b82789592f41ed92fee72557428c4b4ac1213c
moraisgabri <gabriel8azevedo@gmail.com>
2020-10-08 17:00:25 -0300

    usign object destructuring and default param

--- bloco_8/8.4/exercise6.js
+++ bloco_8/8.4/exercise6.js
@@ -4,7 +4,6 @@ const palio = ["Palio", "Fiat", 2019]
 const shelbyCobra = ["Shelby Cobra", "Ford", 1963]
 const chiron = ["Chiron", "Bugatti", 2016]
 
-// escreva toObject abaixo
 const toObject = (array) => {
   const [name, brand, year] = array
   return {
