467d27d8d5dac75ca463b17b65ecc76deb46f620
moraisgabri <gabriel8azevedo@gmail.com>
2020-11-05 15:00:29 -0300

    hanges and react first steps

---
+++ intro_webDev/bloco_8/8.2/exercise7.js
@@ -0,0 +1,88 @@
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+const expected_result = 'O Senhor dos Anéis';
+
+function authorWith3DotsOnName() {
+  const authorArray = books.map(e => e.author.name)
+  let authorWith3DotsOnName;
+  authorArray.forEach(element => {
+    const arrayOfLetters = element.split('')
+    let count = 0;
+    for (let index = 0 ;  index < arrayOfLetters.length; index += 1) {
+      if (arrayOfLetters[index] === '.') {
+        count += 1;
+      }
+    }
+    if (count === 3) {
+      authorWith3DotsOnName = element
+      return authorWith3DotsOnName
+    }
+  })
+  const authorsBook = books.find(element => element.author.name === authorWith3DotsOnName).name
+  return authorsBook
+}
+
+assert.deepStrictEqual(authorWith3DotsOnName(), expected_result);
